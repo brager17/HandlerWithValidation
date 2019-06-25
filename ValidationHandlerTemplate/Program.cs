9eed587dd49140b741d5f24442da257034252453
Евгений <eter55@yandex.ru>
2019-06-25 06:29:08 +0300

    initi

---
+++ ValidationHandlerTemplate/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace ValidationHandlerTemplate
+{
+    public class TIn
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int? Age { get; set; }
+    }
+
+    public class TOut
+    {
+        public string TypeName;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface IHandler<in TIn, out TOut>
+    {
+        TOut Handle(TIn input);
+    }
+
+    public interface IValidator<T>
+    {
+        IEnumerable<ValidationResult> Validate(T obj);
+    }
+
+    public interface IAsyncHandler<T, T1>
+    {
+        Task<T1> AsyncHandle(T input);
+    }
+
+    public interface IAsyncEnumerabeValidator<T>
+    {
+        IAsyncEnumerable<ValidationResult> Validate(T obj);
+    }
+
+    public interface IAsyncValidator<T>
+    {
+        Task<IEnumerable<ValidationResult>> Validate(T obj);
+    }
+}
